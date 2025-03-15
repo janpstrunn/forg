6f72159ab81f9f0e148f705028e18c3fd713bf50
Janpstrunn <janpstrunn@disroot.org>
2025-03-15 13:34:11 -0300

    feat: Rename forg-makedir, and add support for listing directories

--- src/forg-config.sh
+++ src/forg-config.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 config="$HOME/.local/share/forg.conf" # Sources directories
-output_directory="$1"
+output_directory="$2"
+mode=$1
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 list"
+  echo "Usage: $0 create [directory]"
+fi
 
 if [[ ! -f "$config" ]]; then
   echo "Error: extensions.conf or directories.conf not found.  Exiting." >&2
@@ -19,9 +25,15 @@ fi
 mapfile -t directories < <(awk -F'[])=]' '/\[[^]]+\]=/ {print $NF}' "$config" | sort -u)
 
 function main() {
-  for dest_dir in "${directories[@]}"; do
-    [[ -d "$dest_dir" ]] || mkdir -p "$output_directory/$dest_dir"
-  done
+  if [ "$mode" = "create" ]; then
+    for dest_dir in "${directories[@]}"; do
+      [[ -d "$dest_dir" ]] || mkdir -p "$output_directory/$dest_dir"
+    done
+  elif [ "$mode" = "list" ]; then
+    for dir in "${directories[@]}"; do
+      echo "$dir"
+    done
+  fi
 }
 
 main
