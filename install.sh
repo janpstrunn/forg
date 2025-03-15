828eea6b11f2165f9b58eddbd95e48c55eb2e6fe
Janpstrunn <janpstrunn@disroot.org>
2025-03-15 13:25:23 -0300

    chore: Update new config file name

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 echo "Granting executable permission to forg"
 chmod u+x forg.sh
 echo "Moving files"
-move ./config/* "$HOME/.local/share/" && echo "Moved config files to $HOME/.local/share/"
+move ./config/forg.conf "$HOME/.local/share/" && echo "Moved config files to $HOME/.local/share/"
 move ./src/file-organizer.sh "$HOME/.local/bin/" && echo "Moved script to $HOME/.local/bin/"
