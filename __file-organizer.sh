057ce75abba1205a5b3fa948696241867e7ff7c0
Janpstrunn <janpstrunn@disroot.org>
2025-03-15 11:12:10 -0300

    refactor!: Add better logging, error handling, and improve organize methods

--- __file-organizer.sh
+++ __file-organizer.sh
@@ -1,17 +1,127 @@
 #!/usr/bin/env bash
 
+# Use `set -euo pipefail` to catch errors early and prevent unexpected behavior.
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error.
+# -o pipefail: If a command in a pipeline fails, the pipeline's exit status is that of the failing command.
+set -euo pipefail
+
 src_directory="$1"
 output_directory="$2"
-config_ext="$HOME/.local/share/organizer-extensions.conf"
-config_dir="$HOME/.local/share/organizer-directories.conf"
+config_ext="$HOME/.local/share/organizer-extensions.conf"  # Sources filenames and extensions
+config_dir="$HOME/.local/share/organizer-directories.conf" # Sources directories
 logs="$HOME/.cache/organizer.logs"
-time=$(date +%F)
+KILLDUPLICATE=false
+DRYMODE=false
 
+readonly config_ext
+readonly config_dir
+
+function help() {
+  cat <<EOF
+File Organizer
+Usage: $0 [source-directory] [dest-directory] [options]
+Available Options:
+- dry                - Give a preview of the command
+- rmdup              - Remove duplicate files
+- help               - Display this message and exits
+EOF
+}
+
+function directory_check() {
+  # directories is sourced from config_dir
+  for dest_dir in "${directories[@]}"; do
+    [[ -d "$dest_dir" ]] || mkdir -p "$output_directory/$dest_dir"
+  done
+}
+
+move() {
+  find "$src_directory" -type f -print0 | while IFS= read -r -d $'\0' file; do
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+    destination_subpath=""
+
+    case "$method" in
+    broad)
+      destination_subpath="${extension_map[$extension]}"
+      ;;
+    gallery)
+      for pattern in "${!gallery_map[@]}"; do
+        if [[ "$filename" == $pattern ]]; then
+          destination_subpath="${gallery_map[$pattern]}"
+          break
+        fi
+      done
+      ;;
+    esac
+
+    # Ensure destination_dir is valid
+    if [[ -n "$destination_subpath" ]]; then
+      destination_dir="$output_directory/$destination_subpath"
+    else
+      destination_dir="$output_directory/others"
+    fi
+
+    # Check if the file already exists in the destination
+    if [[ -e "$destination_dir/$filename" ]]; then
+      echo "File '$filename' already exists in '$destination_dir'! Possible duplicate?"
+      if [ "$KILLDUPLICATE" = "true" ]; then
+        rm "$src_directory/$filename"
+      fi
+      continue
+    fi
+
+    # Move the file
+    if [ "$DRYMODE" = "true" ]; then
+      echo "Move file: $file ==> $destination_dir"
+    else
+      if mv -n "$file" "$destination_dir" &>"$logs"; then
+        echo "Moved '$filename' to '$destination_dir'"
+      else
+        echo "Error moving '$filename' to '$destination_dir'.  Check '$logs' for details." >&2
+      fi
+    fi
+  done
+  echo "Complete!"
+}
+
+function main() {
+  echo "Choose an organize method:"
+  echo "broad - Apply changes based on filetype"
+  echo "gallery - Apply image rules"
+  echo "music - Apply music rules"
+  echo ""
+  read method
+
+  if [ "$DRYMODE" = "true" ]; then
+    echo "Dry Mode:"
+  fi
+
+  move
+}
+
+# Check for optional arguments
+case "$3" in
+dry)
+  DRYMODE=true
+  ;;
+rmdup)
+  KILLDUPLICATE=true
+  ;;
+help)
+  help
+  exit 0
+  ;;
+esac
+
+# Check required arguments
 if [ -z "$src_directory" ] || [ -z "$output_directory" ]; then
-  echo "Usage: $0 [source-directory] [dest-directory]"
+  echo "Error: Missing required arguments"
+  help
   exit 1
 fi
 
+# Same directory warning
 if [ "$src_directory" == "$output_directory" ]; then
   echo "Be careful! You are running this script in the same directory!"
   echo "If this directory is overpopulated, it may take longer"
@@ -23,89 +133,21 @@ if [ "$src_directory" == "$output_directory" ]; then
   done
 fi
 
-if [ ! -f "$config_ext" ] || [ ! -f "$config_dir" ]; then
-  echo "Error: extensions.conf not found.  Exiting." >&2
+# Check for config files and source them
+if [[ ! -f "$config_ext" ]] || [[ ! -f "$config_dir" ]]; then
+  echo "Error: extensions.conf or directories.conf not found.  Exiting." >&2
   exit 1
 else
-  source "$HOME/.local/share/organizer-extensions.conf"
-  source "$HOME/.local/share/organizer-directories.conf"
+  # shellcheck disable=SC1090
+  source "$config_ext"
+  # shellcheck disable=SC1090
+  source "$config_dir"
   val=$?
   if [ "$val" -ne 0 ]; then
     echo "Failed to source configuration files"
     exit 1
   fi
 fi
-
-# Check if the associative array 'extension_map' is defined
-if ! declare -p extension_map >/dev/null; then
-  echo "Error: Associative array 'extension_map' not defined in extension_map.conf" >&2
-  exit 1
-fi
-
-function directory_check() {
-  for dir in "${directories[@]}"; do
-    [[ -d "$dir" ]] || mkdir -p "$dir"
-  done
-}
-
-function broad() {
-
-  find "$src_directory" -type f -print0 | while IFS= read -r -d $'\0' file; do
-    filename=$(basename "$file")
-    extension="${filename##*.}" # Get extension
-    destination_dir="${extension_map[$extension]}"
-
-    # Handle special cases based on filename
-    if [[ "$filename" == wppr-* || "$filename" == wallpaper-* || "$filename" == wall-* ]]; then
-      destination_dir="$output_directory/media/wallpapers/"
-    elif [[ "$filename" == screen-* || "$filename" == screenshot-* || "$filename" == scrsht-* ]]; then
-      destination_dir="$output_directory/media/screenshots/"
-    elif [[ "$filename" == meme-* ]]; then
-      destination_dir="$output_directory/media/memes/"
-    elif [[ "$filename" == legal-* || "$filename" == contract-* || "$filename" == agreement-* || "$filename" == law-* || "$filename" == case-* ]]; then
-      destination_dir="$output_directory/docs/legal/"
-    elif [[ "$filename" == backup-* || "$filename" == *.bak || "$filename" == *.bkp || "$filename" == *.old || "$filename" == *.sav ]]; then
-      destination_dir="$output_directory/backups/"
-    elif [[ "$filename" == project-* || "$filename" == *.xcf || "$filename" == *.workspace || "$filename" == *.sln || "$filename" == *.xcodeproj ]]; then
-      destination_dir="$output_directory/projects/"
-    elif [[ "$filename" == template-* || "$filename" == tpt-* ]]; then
-      destination_dir="$output_directory/templates/"
-    fi
-
-    if [ -z "$destination_dir" ]; then
-      destination_dir="$output_directory/others/"
-    fi
-
-    # Check if the file already exists in the destination
-    if [[ -e "$destination_dir/$filename" ]]; then
-      continue
-    fi
-
-    # Move the file
-    if mv "$file" "$destination_dir"; then
-      echo "$time Moved '$filename' to '$destination_dir'" >>"$logs"
-      echo "Moved '$filename' to '$destination_dir'"
-    else
-      echo "$time Error moving '$filename' to '$destination_dir'" >>"$logs"
-      echo "Error moving '$filename' to '$destination_dir'" >&2
-    fi
-  done
-  echo "Complete!"
-}
-
-function main() {
-  echo "Choose an organize method:"
-  echo "broad - Apply changes based on filetype"
-  echo "note - Apply note-taking rules"
-  echo "gallery - Apply image rules"
-  echo "music - Apply music rules"
-  read method
-  case "$method" in
-  broad)
-    broad
-    ;;
-  esac
-}
 
 directory_check
 main
